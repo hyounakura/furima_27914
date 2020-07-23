13a494d1bc33ec263523921dd04539dcfb19b51d
hyounakura <leopardtender@gmail.com>
2020-07-23 11:03:19 +0900

    CollectedAddDevise2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
   full_widht = /\A[一-龥ぁ-ん]/
   name_kana = /\A[ァ-ヶー－]+\z/
-  alp_int = (/\A[a-z]+[0-9]+\z/)
+  alp_int = /\A[a-z]+[0-9]+\z/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :name, uniqueness: { case_sensitive: true }
-    validates :family_name, format: { with: full_widht, message: 'Full-width characters'}
-    validates :first_name, format: { with: full_widht, message: 'Full-width characters'}
-    validates :family_name_kana, format: { with: name_kana, message: 'Full-width katakana characters'}
-    validates :first_name_kana, format: { with: name_kana, message: 'Full-width katakana characters'}
+    validates :family_name, format: { with: full_widht, message: 'Full-width characters' }
+    validates :first_name, format: { with: full_widht, message: 'Full-width characters' }
+    validates :family_name_kana, format: { with: name_kana, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: name_kana, message: 'Full-width katakana characters' }
     validates :birthday_id
-    validates :password, format: { with: alp_int, message: 'Password Include both letters and numbers'}
+    validates :password, format: { with: alp_int, message: 'Password Include both letters and numbers' }
   end
 end
