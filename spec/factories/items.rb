e38ac0738269f3efdb0e9b925f4805fbe2cfe121
hyounakura <leopardtender@gmail.com>
2020-07-25 17:23:55 +0900

    CollectedAddItems

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    image { File.open("#{Rails.root}/public/images/test_image.jpg") }
+    # image { File.open("#{Rails.root}/public/images/test_image.jpg") }
     name { 'バッグ' }
     explanation { '去年買いました。' }
     price { 5000 }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,6 +9,6 @@ FactoryBot.define do
     first_name { '太郎' }
     family_name_kana { 'スズキ' }
     first_name_kana { 'タロウ' }
-    birthday_id { 1993 - 0o3 - 20 }
+    birthday_id { '1993-03-20' }
   end
 end
