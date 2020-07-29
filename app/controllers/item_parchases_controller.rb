f609aa072a8d76d90174132ed3fe4b27222d780d
hyounakura <leopardtender@gmail.com>
2020-07-29 21:35:21 +0900

    Collected

--- app/controllers/item_parchases_controller.rb
+++ app/controllers/item_parchases_controller.rb
@@ -1,6 +1,6 @@
 class ItemParchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action set_item, only: {:index, :create}
+  before_action :set_item, only: [:index, :create]
 
   def index
     redirect_to root_path if current_user.id == @item.user_id || @item.item_parchase.present?
@@ -18,12 +18,12 @@ class ItemParchasesController < ApplicationController
     end
   end
 
+  
+  private
   def set_item
     @item = Item.find(params[:item_id])
   end
-
-  private
-
+  
   def item_address_params
     params.require(:item_address).permit(:postal_code, :area_id, :city, :address, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
